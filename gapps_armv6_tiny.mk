c5ee0cffe199e117f400a75524956bb9b3da38be
Pavel Kirpichyov <pavel.kirpichyov@gmail.com>
2012-12-21 11:48:22 +0200

    Add tiny armv6 gapps conf

---
+++ gapps_armv6_tiny.mk
@@ -0,0 +1,14 @@
+# Google Apps
+PRODUCT_COPY_FILES += \
+	vendor/google/armv6/app/Gmail.apk:system/app/Gmail.apk \
+	vendor/google/armv6/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+	vendor/google/armv6/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+	vendor/google/armv6/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+	vendor/google/armv6/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+	vendor/google/armv6/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+	vendor/google/armv6/app/PlayStore.apk:system/app/PlayStore.apk \
+	vendor/google/armv6/app/NetworkLocation.apk:system/app/NetworkLocation.apk
+
+PRODUCT_COPY_FILES += \
+        vendor/google/armv6/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+        vendor/google/armv6/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml
