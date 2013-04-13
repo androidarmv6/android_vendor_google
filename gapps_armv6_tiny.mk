e2f76507bcd8c8ec12241378b070c772fb1505ad
AndroidMeda <hephappy@gmail.com>
2013-04-13 11:13:33 +0300

    Uninstall gapps unless MINI_GAPPS=true
    Change-Id: I19b65b57678586c382e42ba674f0492605e13df4

--- gapps_armv6_tiny.mk
+++ gapps_armv6_tiny.mk
@@ -1,4 +1,7 @@
 # Google Apps
+
+ifeq ($(MINI_GAPPS),true)
+ # install
 PRODUCT_COPY_FILES += \
 	vendor/google/armv6/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
 	vendor/google/armv6/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
@@ -12,8 +15,28 @@ PRODUCT_COPY_FILES += \
 	vendor/google/armv6/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk
 
 PRODUCT_COPY_FILES += \
-        vendor/google/armv6/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-        vendor/google/armv6/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml
+	vendor/google/armv6/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+	vendor/google/armv6/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml
 
 PRODUCT_COPY_FILES += \
 	vendor/google/armv6/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar
+
+else
+# uninstall
+$(shell rm -f $(OUT)/system/app/GoogleBackupTransport.apk)
+$(shell rm -f $(OUT)/system/app/GoogleCalendarSyncAdapter.apk)
+$(shell rm -f $(OUT)/system/app/GoogleContactsSyncAdapter.apk)
+$(shell rm -f $(OUT)/system/app/GoogleLoginService.apk)
+$(shell rm -f $(OUT)/system/app/GoogleServicesFramework.apk)
+$(shell rm -f $(OUT)/system/app/PlayStore.apk)
+$(shell rm -f $(OUT)/system/app/SetupWizard.ap)
+$(shell rm -f $(OUT)/system/app/NetworkLocation.apk)
+$(shell rm -f $(OUT)/system/app/Talk.apk)
+$(shell rm -f $(OUT)/system/app/ChromeBookmarksSyncAdapter.apk)
+
+$(shell rm -f $(OUT)/system/etc/permissions/com.google.android.maps.xml)
+$(shell rm -f $(OUT)/system/etc/permissions/com.google.android.media.effects.xml)
+
+$(shell rm -f $(OUT)/system/framework/com.google.android.maps.jar)
+
+endif
